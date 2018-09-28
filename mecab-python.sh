2ed27851591efa5cd5fee4a6408f85803bdd7384
Joon <wjl0316@naver.com>
2018-09-29 04:02:34 +0900

    package.json changed for mecab-python

---
+++ mecab-python.sh
@@ -0,0 +1,13 @@
+# install mecab-python
+cd /tmp
+git clone https://bitbucket.org/eunjeon/mecab-python-0.996.git
+cd mecab-python-0.996
+
+python setup.py build
+python setup.py install
+
+if hash "python3" &>/dev/null
+then
+    python3 setup.py build
+    python3 setup.py install
+fi
