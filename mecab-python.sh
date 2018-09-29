a22df66f6639650a208a27e98d95ab09bda3de57
Joon <wjl0316@naver.com>
2018-09-29 17:55:46 +0900

    mecab-python; mecab-config

--- mecab-python.sh
+++ mecab-python.sh
@@ -2,31 +2,6 @@
 cd /tmp
 git clone https://bitbucket.org/eunjeon/mecab-python-0.996.git
 cd mecab-python-0.996
-> setup.py
-
-cat <<EOT >> setup.py
-#!/usr/bin/env python
-
-from distutils.core import setup,Extension,os
-import string
-
-def cmd1(str):
-    return os.popen(str).readlines()[0][:-1]
-
-def cmd2(str):
-    return cmd1(str).split()
-
-setup(name = "mecab-python",
-	version = "0.996/ko-0.9.0",
-	py_modules=["MeCab"],
-	ext_modules = [
-		Extension("_MeCab",
-			["MeCab_wrap.cxx",],
-			include_dirs="/usr/local/include".split(),
-			library_dirs="/usr/local/lib".split(),
-			libraries="mecab stdc++".split())
-	])
-EOT
 
 python setup.py build
 python setup.py install
