545dbf131ea1a133ca8daf1ffba4966cbea75470
Joon <wjl0316@naver.com>
2018-09-29 16:54:12 +0900

    mecab-python nth Trial

--- mecab-python.sh
+++ mecab-python.sh
@@ -1,13 +1,6 @@
-# install mecab-python
 cd /tmp
 git clone https://bitbucket.org/eunjeon/mecab-python-0.996.git
 cd mecab-python-0.996
-
-python setup.py build
-python setup.py install
-
-if hash "python3" &>/dev/null
-then
-    python3 setup.py build
-    python3 setup.py install
-fi
+python3 setup.py build
+su
+python3 setup.py install
