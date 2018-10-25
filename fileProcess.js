b771aa67d7d5405d5c7eb78ff606543854edcf9c
sni-J <wjl0316@naver.com>
2018-10-25 16:20:27 +0900

    AWS applying

--- fileProcess.js
+++ fileProcess.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 var mkdirp = require('mkdirp');
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
-const S3_BUCKET = process.env.S3_BUCKET;
+const S3_BUCKET = process.env.S3_BUCKET_NAME;
 aws.config.region = 'ap-northeast-2';
 
 var fileProcess = this;
